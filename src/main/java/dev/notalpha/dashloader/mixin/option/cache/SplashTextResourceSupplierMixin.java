351354c694ea517434735cd3a4fdc070119b8114
Malionaro <Alexander_zurek@gmx.de>
2026-09-19 23:01:57 +0200

    Fix splash ClassCastException (Component <-> String conversion)

--- src/main/java/dev/notalpha/dashloader/mixin/option/cache/SplashTextResourceSupplierMixin.java
+++ src/main/java/dev/notalpha/dashloader/mixin/option/cache/SplashTextResourceSupplierMixin.java
@@ -9,6 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 import net.minecraft.client.resources.SplashManager;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
 
@@ -19,18 +20,20 @@ public class SplashTextResourceSupplierMixin {
 			at = @At(value = "HEAD"),
 			cancellable = true
 	)
-	private void applySplashCache(ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfoReturnable<List<String>> cir) {
-		SplashModule.TEXTS.visit(CacheStatus.LOAD, cir::setReturnValue);
+	private void applySplashCache(ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfoReturnable<List<Component>> cir) {
+		SplashModule.TEXTS.visit(CacheStatus.LOAD, strings -> cir.setReturnValue(strings.stream().map(Component::literal).toList()));
 	}
 
 	@Inject(
 			method = "prepare(Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)Ljava/util/List;",
 			at = @At(value = "RETURN")
 	)
-	private void stealSplashCache(ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfoReturnable<List<String>> cir) {
+	private void stealSplashCache(ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfoReturnable<List<Component>> cir) {
 		SplashModule.TEXTS.visit(CacheStatus.SAVE, strings -> {
 			strings.clear();
-			strings.addAll(cir.getReturnValue());
+			for (Component component : cir.getReturnValue()) {
+				strings.add(component.getString());
+			}
 		});
 	}
 }

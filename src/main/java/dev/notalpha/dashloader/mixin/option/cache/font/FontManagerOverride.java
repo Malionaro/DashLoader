11e9803d0eb45e2a6e513cecb4ee61a05c61292f
Malionaro <Alexander_zurek@gmx.de>
2026-09-20 01:21:38 +0200

    Fix Default-font crash on LOAD: fall back to vanilla when cache lacks default font

--- src/main/java/dev/notalpha/dashloader/mixin/option/cache/font/FontManagerOverride.java
+++ src/main/java/dev/notalpha/dashloader/mixin/option/cache/font/FontManagerOverride.java
@@ -4,6 +4,7 @@ import dev.notalpha.dashloader.DashLoader;
 import dev.notalpha.dashloader.api.cache.CacheStatus;
 import dev.notalpha.dashloader.client.font.FontModule;
 import dev.notalpha.dashloader.mixin.accessor.FontManagerProviderIndexAccessor;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.FontManager;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.profiler.Profiler;
@@ -25,8 +26,14 @@ public class FontManagerOverride {
 	)
 	private void loadFonts(ResourceManager resourceManager, Executor executor, CallbackInfoReturnable<CompletableFuture<FontManager.ProviderIndex>> cir) {
 		FontModule.DATA.visit(CacheStatus.LOAD, data -> {
-			DashLoader.LOG.info("Providing fonts");
-			cir.setReturnValue(CompletableFuture.completedFuture(FontManagerProviderIndexAccessor.create(data.providers, data.allProviders)));
+			// A cache without the default font (e.g. saved while every font was skipped)
+			// breaks vanilla font loading - fall back instead.
+			if (data != null && data.providers.containsKey(MinecraftClient.DEFAULT_FONT_ID)) {
+				DashLoader.LOG.info("Providing fonts");
+				cir.setReturnValue(CompletableFuture.completedFuture(FontManagerProviderIndexAccessor.create(data.providers, data.allProviders)));
+			} else {
+				DashLoader.LOG.warn("Font cache missing default font, falling back to vanilla loading");
+			}
 		});
 	}
 

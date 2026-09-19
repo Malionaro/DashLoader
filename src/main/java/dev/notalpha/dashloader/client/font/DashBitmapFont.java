276badbabd3c3dc70f3083d26405347bd744d5b9
Malionaro <Alexander_zurek@gmx.de>
2026-09-20 00:22:46 +0200

    Fix 26.1 bitmap fonts (native shape, no reflection) + per-font save resilience

--- src/main/java/dev/notalpha/dashloader/client/font/DashBitmapFont.java
+++ src/main/java/dev/notalpha/dashloader/client/font/DashBitmapFont.java
@@ -10,34 +10,29 @@ import net.minecraft.client.gui.font.CodepointMap;
 import net.minecraft.client.gui.font.providers.BitmapProvider;
 
 public final class DashBitmapFont implements DashObject<BitmapProvider, BitmapProvider> {
-	public final int imageId;
 	public final int image;
 	public final IntObjectList<DashBitmapFontGlyph> glyphs;
 
-	public DashBitmapFont(int imageId, int image,
+	public DashBitmapFont(int image,
 	                      IntObjectList<DashBitmapFontGlyph> glyphs) {
-		this.imageId = imageId;
 		this.image = image;
 		this.glyphs = glyphs;
 	}
 
 	public DashBitmapFont(BitmapProvider bitmapFont, RegistryWriter writer) {
 		BitmapFontAccessor font = ((BitmapFontAccessor) bitmapFont);
-		Object holder = BitmapFontReflection.getImageData(bitmapFont);
-		this.imageId = writer.add(BitmapFontReflection.getIdentifier(holder));
-		this.image = writer.add(BitmapFontReflection.getImage(holder));
+		this.image = writer.add(font.getImage());
 		this.glyphs = new IntObjectList<>(new ArrayList<>());
-		font.getGlyphs().forEach((integer, bitmapFontGlyph) -> this.glyphs.put(integer, new DashBitmapFontGlyph(bitmapFontGlyph)));
+		font.getGlyphs().forEach((integer, bitmapFontGlyph) -> this.glyphs.put(integer, new DashBitmapFontGlyph(bitmapFontGlyph, writer)));
 	}
 
 	public BitmapProvider export(RegistryReader reader) {
-		Object holder = BitmapFontReflection.newHolder(reader.get(this.imageId), reader.get(this.image));
 		CodepointMap<BitmapProvider.Glyph> out = new CodepointMap<>(
 				BitmapProvider.Glyph[]::new,
 				BitmapProvider.Glyph[][]::new
 		);
-		this.glyphs.forEach((key, value) -> out.put(key, value.export(holder)));
-		return BitmapFontReflection.newProvider(holder, out);
+		this.glyphs.forEach((key, value) -> out.put(key, value.export(reader)));
+		return BitmapFontAccessor.init(reader.get(this.image), out);
 	}
 
 	@Override
@@ -47,15 +42,13 @@ public final class DashBitmapFont implements DashObject<BitmapProvider, BitmapPr
 
 		DashBitmapFont that = (DashBitmapFont) o;
 
-		if (imageId != that.imageId) return false;
 		if (image != that.image) return false;
 		return glyphs.equals(that.glyphs);
 	}
 
 	@Override
 	public int hashCode() {
-		int result = imageId;
-		result = 31 * result + image;
+		int result = image;
 		result = 31 * result + glyphs.hashCode();
 		return result;
 	}

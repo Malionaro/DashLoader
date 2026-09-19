0390a7cae779e63009fe63d667d07921764638de
Malionaro <Alexander_zurek@gmx.de>
2026-09-19 22:32:11 +0200

    Fix 26.2 KeyboardMixin (KeyEvent API) + AffineTransformation shadow type

--- src/main/java/dev/notalpha/dashloader/mixin/main/KeyboardMixin.java
+++ src/main/java/dev/notalpha/dashloader/mixin/main/KeyboardMixin.java
@@ -4,12 +4,10 @@ import dev.notalpha.dashloader.DashLoader;
 import dev.notalpha.dashloader.api.cache.CacheStatus;
 import dev.notalpha.dashloader.client.DashLoaderClient;
 import net.minecraft.client.KeyboardHandler;
-import org.lwjgl.glfw.GLFW;
+import net.minecraft.client.input.KeyEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
@@ -17,31 +15,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  */
 @Mixin(KeyboardHandler.class)
 public class KeyboardMixin {
-	@Unique
-	private boolean shiftHeld = false;
-
 	@Inject(
 			method = "handleDebugKeys",
 			at = @At(
 					value = "INVOKE",
-					target = "Lnet/minecraft/client/Minecraft;reloadResourcePacks()Ljava/util/concurrent/CompletableFuture;",
-					shift = At.Shift.BEFORE
+					target = "Lnet/minecraft/client/Minecraft;reloadResourcePacks()Ljava/util/concurrent/CompletableFuture;"
 			)
 	)
-	private void f3tReloadWorld(int key, CallbackInfoReturnable<Boolean> cir) {
-		if (!this.shiftHeld) {
+	private void f3tReloadWorld(KeyEvent event, CallbackInfoReturnable<Boolean> cir) {
+		if (!event.hasShiftDown()) {
 			if (DashLoaderClient.CACHE.getStatus() == CacheStatus.IDLE) {
 				DashLoader.LOG.info("Clearing cache.");
 				DashLoaderClient.CACHE.remove();
 			}
 		}
-	}
-
-	@Inject(
-			method = "keyPress",
-			at = @At("HEAD")
-	)
-	private void keyPress(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
-		this.shiftHeld = action != 0 && modifiers == GLFW.GLFW_MOD_SHIFT;
 	}
 }

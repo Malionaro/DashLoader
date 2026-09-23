a5a421e929daeb681b93379d5790d4f51ba81a66
Malionaro <Alexander_zurek@gmx.de>
2026-09-23 21:33:26 +0200

    TEMP-DIAG step 4

--- src/main/java/dev/notalpha/dashloader/neoforge/DashLoaderNeoForge.java
+++ src/main/java/dev/notalpha/dashloader/neoforge/DashLoaderNeoForge.java
@@ -19,8 +19,8 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 @Mod(value = "dashloader", dist = Dist.CLIENT)
 public class DashLoaderNeoForge {
 	public DashLoaderNeoForge(ModContainer container) {
-		// TEMP-DIAG step 3: extension point + LOG, still no CACHE
+		// TEMP-DIAG step 4: force DashLoader statics (VERSION, MOD_HASH, Hyphen serializer)
 		container.registerExtensionPoint(IConfigScreenFactory.class, new DashLoaderConfigScreenFactory());
-		DashLoader.LOG.info("DL-NEO STEP3");
+		DashLoader.LOG.info("DL-NEO STEP4 hash={}", DashLoader.MOD_HASH);
 	}
 }

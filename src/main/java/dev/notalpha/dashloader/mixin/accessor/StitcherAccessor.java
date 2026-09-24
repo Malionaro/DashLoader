49a0738e02faa72bc5656bf48be4c421e1d797a9
Malionaro <Alexander_zurek@gmx.de>
2026-09-24 23:10:36 +0200

    Author tag Malionaro on new files

--- src/main/java/dev/notalpha/dashloader/mixin/accessor/StitcherAccessor.java
+++ src/main/java/dev/notalpha/dashloader/mixin/accessor/StitcherAccessor.java
@@ -4,6 +4,10 @@ import net.minecraft.client.texture.TextureStitcher;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+/**
+ * @author Malionaro
+ */
+
 @Mixin(TextureStitcher.class)
 public interface StitcherAccessor {
 	@Accessor
